f677a9359ac2a13bf55cf32c78e3d6c6efb8df60
Sergey Bondarchuk <roislerxs@gmail.com>
2024-01-12 11:58:46 +0700

    feat: add questionList.js

---
+++ hangman/assets/questionList.js
@@ -0,0 +1,84 @@
+const questionList = [
+  {
+      answer: "guitar",
+      question: "A musical instrument with strings."
+  },
+  {
+      answer: "oxygen",
+      question: "A colorless, odorless gas essential for life."
+  },
+  {
+      answer: "mountain",
+      question: "A large natural elevation of the Earth's surface."
+  },
+  {
+      answer: "painting",
+      question: "An art form using colors on a surface to create images or expression."
+  },
+  {
+      answer: "astronomy",
+      question: "The scientific study of celestial objects and phenomena."
+  },
+  {
+      answer: "football",
+      question: "A popular sport played with a spherical ball."
+  },
+  {
+      answer: "chocolate",
+      question: "A sweet treat made from cocoa beans."
+  },
+  {
+      answer: "butterfly",
+      question: "An insect with colorful wings and a slender body."
+  },
+  {
+      answer: "history",
+      question: "The study of past events and human civilization."
+  },
+  {
+      answer: "pizza",
+      question: "A savory dish consisting of a round, flattened base with toppings."
+  },
+  {
+      answer: "jazz",
+      question: "A genre of music characterized by improvisation and syncopation."
+  },
+  {
+      answer: "camera",
+      question: "A device used to capture and record images or videos."
+  },
+  {
+      answer: "diamond",
+      question: "A precious gemstone known for its brilliance and hardness."
+  },
+  {
+      answer: "adventure",
+      question: "An exciting or daring experience."
+  },
+  {
+      answer: "science",
+      question: "The systematic study of the structure and behavior of the physical and natural world."
+  },
+  {
+      answer: "bicycle",
+      question: "A human-powered vehicle with two wheels."
+  },
+  {
+      answer: "sunset",
+      question: "The daily disappearance of the sun below the horizon."
+  },
+  {
+      answer: "coffee",
+      question: "A popular caffeinated beverage made from roasted coffee beans."
+  },
+  {
+      answer: "dance",
+      question: "A rhythmic movement of the body often performed to music."
+  },
+  {
+      answer: "galaxy",
+      question: "A vast system of stars, gas, and dust held together by gravity."
+  },
+];
+
+export default questionList;
